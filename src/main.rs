1e04cd8bf458dcd079f55795735faab397ebc76c
nmetens <metens@pdx.edu>
2025-02-11 10:49:44 -0800

    Added sqlx crate to project and changed main method to be asynch

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ mod job; // References job.rs file
 mod application; // References application.rs file
 mod list; // For saving list data to a file
 
-fn main() {
+#[tokio::main] // Requires the `attributes` feature of `async-std`
+async fn main() -> Result<(), sqlx::Error> {
     let mut apps = application::Applications::new();
     apps.add_job();
     apps.view_apps();
 
     // Output all jobs to a file in 'output' directory:
     let _ = list::print_list(apps); 
+
+    Ok(())
 }
