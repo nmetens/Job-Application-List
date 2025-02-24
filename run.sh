20b1b5bb4e73885ed59d8ea5fc06d7bf8ceeaf8d
nmetens <metens@pdx.edu>
2025-02-24 11:36:43 -0800

    Added a bash script for faster running

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# To import cargo for rust into project repo:
+. "$HOME/.cargo/env"
+
+# Commands to run the project and build:
+#cargo build
+
+#cargo fmt
+
+#cargo clippy
+
+cargo run
+
+#cargo test
